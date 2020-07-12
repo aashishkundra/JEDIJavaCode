556e3dd81b6f8241f1b9ba616c2923c7fd558fdf
aashishkundra <aashishkundra13@gmail.com>
2020-07-12 21:32:56 +0530

    First commit: 	Added the class skeleton to the file Bicycle.java. 	1. It is evident to make Gear and Bicycle a class. 	2. The Rim, Tyre and Wheel are made classes since they have their own variables and they can be used independent of bicycle, and having an open mind about future additions which can't be predicted this looks an optimal choice for now.

---
+++ Bicycle.java
@@ -0,0 +1,28 @@
+package javaAssignment;
+
+import java.lang.*;
+import java.util.*;
+
+
+class Gear {
+
+}
+
+class Rim {
+
+}
+
+class Tyre {
+    
+}
+
+class Wheel {
+
+}
+
+class Bicycle {
+    public static void main(String args[]) {
+
+    }
+}
+
