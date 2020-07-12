00ac92e6ea0ec88c264fdc153c01e14851af3df1
aashishkundra <aashishkundra13@gmail.com>
2020-07-12 21:44:35 +0530

    Feature added: 	1. Added instance variables for each class that they should have. 	2. Initialised constructors for each class.

--- Bicycle.java
+++ Bicycle.java
@@ -5,22 +5,49 @@ import java.util.*;
 
 
 class Gear {
+    private double chainRingSize; 
+    private double cogSize;
 
+    Gear() {
+
+    }
+
+    
 }
 
 class Rim {
+    private double size;
+
+    Rim() {
+
+    }
+
 
 }
 
 class Tyre {
-    
+    private double thickness;
+
+    Tyre() {
+
+    }
+
 }
 
 class Wheel {
+    private Tyre tyre;
+    private Rim rim;
+
+    Wheel() {
+
+    }
 
 }
 
 class Bicycle {
+    private Wheel wheel1;
+    private Wheel wheel2;
+    private Gear gear;
     public static void main(String args[]) {
 
     }
